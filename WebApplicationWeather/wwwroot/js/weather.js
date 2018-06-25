4bca6d797717d6ef953ee8e3877d10e96141a27f
davidtodorov <t_david@outlook.com>
2018-06-25 18:03:12 +0300

    Working fine

--- WebApplicationWeather/wwwroot/js/weather.js
+++ WebApplicationWeather/wwwroot/js/weather.js
@@ -17,46 +17,44 @@
 
     $('.search-city-btn').on('click', function (event) {
         var cityName = $('#city-input').val();
-        $('table#weatherTable').css("display", "none");
+        if (localStorage.getItem("cityKey")) {
+            $('table#weatherTable').css("display", "table");
+        } else {
+            $('table#weatherTable').css("display", "none");
+        }
+        
         $.get("/weather/search",
             { Name: cityName },
             function (data) {
-                if (data) {
-                    var stringData = data; 
-                    stringData = '{ "cities": ' + stringData + ' }';
-                    var jsonResult = JSON.parse(stringData);
-                    if (jsonResult.length === 0) {
+                if (data) { 
+                    if (data.length === 0) {
                         alert("No cities found!");
                     }
                     $('ul#cities-list').empty();
-                    jsonResult.cities.forEach(appendCity);
+                    data.forEach(appendCity);
                 } else {
                     alert('No more requests allowed!');
                 }
             });
         event.preventDefault();
     });
+
     function refreshData(timeInterval) {
         
         refresh = setInterval(getCityKey, timeInterval);
     }
 });
 
-function getCityKey() {
-    var cityKey = localStorage.getItem("cityKey");
-    if (cityKey) {
-        weatherDetails(cityKey);
-    }
-}
-
 function appendCity(city) {
     $('ul#cities-list').append(
-        $('<li>City: ' + city.LocalizedName + ' Country: ' + city.Country.LocalizedName + '</li>')
-            .attr({ "data-key": city.Key })
+        $('<li><a>' + city.localizedName + ',' + city.country.localizedName + '</a></li>')
+            .attr({ "data-key": city.key })
             .click(function () {
                 $('ul#cities-list').empty();
                 
-                localStorage.setItem("cityKey", city.Key);
+                localStorage.setItem("cityKey", city.key);
+                localStorage.setItem("cityName", city.localizedName);
+                localStorage.setItem("country", city.country.localizedName);
                 weatherDetails(city.key);
             })
     );
@@ -66,24 +64,21 @@ function weatherDetails(cityKey) {
     $('#weatherTable tbody').empty();
 
     $.get("/weather/city",
-        { cityId: cityKey },
+        { CityId: cityKey },
         function (data) {
-            var stringData = data;
-            var weather = '{ "data": ' + stringData + ' }';
-            var weatherResult = JSON.parse(weather);
-
             $('table#weatherTable').css("display", "table");
+            $('th#weatherForCity').text("Current Weather - " + localStorage.getItem("cityName") +", "+localStorage.getItem("country"));
             $('#weatherTable tbody').append(
                 $('<tr>' +
-                    '<td><img src="https://developer.accuweather.com/sites/default/files/01-s.png"/>' +
+                    '<td><img src="/icons/' + data[0].weatherIcon + '.png"/>' +
                     '<p style="display: inline-block;">' +
-                    weatherResult.data[0].WeatherText +
+                    data[0].weatherText +
                     '</p></td>' +
                     '<td>' +
-                    weatherResult.data[0].Temperature.Metric.Value +
+                    data[0].temperature.metric.value + " " + data[0].temperature.metric.unit +
                     '</td>' +
                     '<td>' +
-                    weatherResult.data[0].RelativeHumidity +
+                    data[0].relativeHumidity +
                     '%</td>' +
                     '</tr>'
                 )
@@ -91,4 +86,11 @@ function weatherDetails(cityKey) {
 
             $('#weatherTable td th').addClass("col-md-4");
         });
+}
+
+function getCityKey() {
+    var cityKey = localStorage.getItem("cityKey");
+    if (cityKey) {
+        weatherDetails(cityKey);
+    }
 }
